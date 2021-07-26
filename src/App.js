019fef986dd276eb07c7b83d50acb0133b93579e
Jakub Jureczko <jakub.jureczko@czek.it>
2021-07-27 01:30:18 +0200

    hash router

--- src/App.js
+++ src/App.js
@@ -3,42 +3,48 @@ import "./App.scss";
 import ArticleList from "./components/ArticleList/ArticleList";
 import ArticleSingle from "./components/ArticleSingle/ArticleSingle";
 import Favourites from "./components/Favourites/Favourites";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  HashRouter,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 import logo from "./assets/img/blogging.png";
 
 const App = (props) => {
   console.log(props);
   return (
-    <Router>
+    <HashRouter basename="/">
       <div className="app">
         <div className="app__header">
-          <Link className="app__anchors" to="/land">
+          <Link className="app__anchors" to="/">
             <h4>HOME</h4>
           </Link>
           <div className="app__logo">
             <img className="app__logo--img" src={logo} alt="" />
             <h3>BLOG</h3>
           </div>
-          <Link className="app__anchors" to="/land/favourites">
+          <Link className="app__anchors" to="/favourites">
             <h3>FAVOURITES</h3>
           </Link>
         </div>
         <div className="app__body">
           <Switch>
-            <Route exact path="/land">
+            <Route exact path="/">
               <ArticleList />
             </Route>
-            <Route path="land/article/:id">
+            <Route path="/article/:id">
               <ArticleSingle />
             </Route>
-            <Route path="land/favourites">
+            <Route path="/favourites">
               <Favourites />
             </Route>
           </Switch>
         </div>
       </div>
-    </Router>
+    </HashRouter>
   );
 };
 
